b01d540abbd14e5cb43e4034a7c36671558dd78b
abdo-ss1 <abdo-ss1@hotmail.com>
2025-07-19 16:22:05 +0300

    Added

---
+++ DailyCheckIn/Controllers/UserProfileController.cs
@@ -0,0 +1,41 @@
+﻿using DailyCheckIn.Data;
+using DailyCheckIn.Models.DTO;
+using DailyCheckIn.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyCheckIn.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ApplicationDbContext _DbContext;
+
+        public UserProfileController(UserManager<AppUser> userManager, ApplicationDbContext DbContext)
+        {
+            _userManager = userManager;
+            _DbContext = DbContext;
+        }
+
+        [HttpGet("{username}")]
+        public IActionResult Index(string userName)
+        {
+            var userProfileDto = _DbContext.Users.Where(user => user.UserName == userName).Select(
+                user => new UserProfileDTO
+                { UserName = user.UserName ?? "",
+                  Name = user.UserName,
+                  ArabicName = user.ArabicName,
+                  Email = user.Email,
+                  PhoneNumber = user.PhoneNumber,
+                  HourlyRate = user.HourlyRate,
+                  Bonus = user.Bonus,
+                  StartDate = user.StartDate
+                }
+            ).FirstOrDefault();
+            if (userProfileDto == null)
+                return NotFound();
+
+            return View(userProfileDto);
+        }
+    }
+}
---
+++ DailyCheckIn/Helpers/AttendanceCalculator.cs
@@ -0,0 +1,88 @@
+﻿using DailyCheckIn.Models.Entities;
+
+namespace DailyCheckIn.Helpers
+{
+    public class AttendanceCalculator
+    {
+        public static double CalculateDialyMinutesWorked(Attendance attendance)
+        {
+            if (!attendance.CheckOut.HasValue)
+                return -1;
+
+            double TotalTimesOffMinutes = 0;
+
+            if (attendance.TimeOffsForTheDay != null) { 
+                foreach (var TimeOff in attendance.TimeOffsForTheDay)
+                {
+                    if(TimeOff.TimeOffEnd.HasValue)
+                        TotalTimesOffMinutes += TimeOff.TimeOffEnd.Value.ToTimeSpan().TotalMinutes - TimeOff.TimeOffStart.ToTimeSpan().TotalMinutes;
+                }
+            }
+
+            if (attendance.Date == DateOnly.FromDateTime(attendance.CheckOut.Value))
+                return attendance.CheckOut.Value.TimeOfDay.TotalMinutes - attendance.CheckIn.ToTimeSpan().TotalMinutes - TotalTimesOffMinutes;
+            else if (attendance.Date > DateOnly.FromDateTime(attendance.CheckOut.Value))
+                return -1;
+            else
+            {
+                var numberOfDays = DateOnly.FromDateTime(attendance.CheckOut.Value).Day - attendance.Date.Day;
+                for (; numberOfDays > 1; numberOfDays--)
+                {
+                    TotalTimesOffMinutes += 60 * 24;
+                }
+
+                var MidNightTime = new TimeOnly(24, 0);
+
+                var TotalOfMinutesAfterMidNight = attendance.CheckOut.Value.TimeOfDay.TotalMinutes;
+
+                var totalOfMinutesTillMidNight = MidNightTime.ToTimeSpan().TotalMinutes - attendance.CheckIn.ToTimeSpan().TotalMinutes;
+
+                return totalOfMinutesTillMidNight + totalOfMinutesTillMidNight;
+            }
+        }
+
+        public static double CalculateDialyMinutesWorked(Attendance attendance, DateTime forNow)
+        {
+            double TotalTimesOffMinutes = 0;
+
+            if (attendance.TimeOffsForTheDay != null)
+            {
+                foreach (var TimeOff in attendance.TimeOffsForTheDay)
+                {
+                    if (TimeOff.TimeOffEnd.HasValue)
+                        TotalTimesOffMinutes += TimeOff.TimeOffEnd.Value.ToTimeSpan().TotalMinutes - TimeOff.TimeOffStart.ToTimeSpan().TotalMinutes;
+                }
+            }
+
+            if (attendance.Date == DateOnly.FromDateTime(forNow))
+                return forNow.TimeOfDay.TotalMinutes - attendance.CheckIn.ToTimeSpan().TotalMinutes - TotalTimesOffMinutes;
+            else if (attendance.Date > DateOnly.FromDateTime(forNow))
+                return -1;
+            else
+            {
+                var numberOfDays = DateOnly.FromDateTime(forNow).Day - attendance.Date.Day;
+                for (; numberOfDays > 1; numberOfDays--)
+                {
+                    TotalTimesOffMinutes += 60 * 24;
+                }
+
+                var MidNightTime = new TimeOnly(24, 0);
+
+                var TotalOfMinutesAfterMidNight = forNow.TimeOfDay.TotalMinutes;
+
+                var totalOfMinutesTillMidNight = MidNightTime.ToTimeSpan().TotalMinutes - attendance.CheckIn.ToTimeSpan().TotalMinutes;
+
+                return totalOfMinutesTillMidNight + totalOfMinutesTillMidNight;
+            }
+                
+
+
+        }
+
+        public static double CalculateDailyMoneyEarned(double MinutesWorked, double HourlyRate)
+        {
+
+            return MinutesWorked * HourlyRate / 60;
+        }
+    }
+}
